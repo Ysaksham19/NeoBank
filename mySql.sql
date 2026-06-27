d4a536c6cd761308eaedc2250e5edcddd5080e7c
Saksham Yadav <158077573+Ysaksham19@users.noreply.github.com>
2026-06-27 13:00:52 +0530

    fix: delete dangerous mySql.sql scratchpad (contained DROP DATABASE commands)

--- mySql.sql
+++ mySql.sql
@@ -1,27 +1,6 @@
-CREATE DATABASE neobank_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-use neobank_db;
-show tables;
-
-select * from account_types ;
-select * from accounts;
-select * from branches,kyc_details;
-select * from otp_verifications ;
-select * from roles;
-select * from user_roles;
-select * from users;
-
-
-DROP TABLE users;
-USE banking_app;
-SHOW TABLES;
-
-
-CREATE DATABASE banking_app;
-
-SELECT * FROM flyway_schema_history;
-SHOW CREATE TABLE flyway_schema_history;
-
-
-select * from flyway_schema_history;
-drop schema neobank_db;
-DROP DATABASE banking_app;
+-- This file has been intentionally cleared.
+-- All database schema is managed exclusively via Flyway migrations
+-- located at: backend/neobank360app/src/main/resources/db/migration/
+--
+-- To inspect the schema, run:
+--   SELECT * FROM flyway_schema_history;
